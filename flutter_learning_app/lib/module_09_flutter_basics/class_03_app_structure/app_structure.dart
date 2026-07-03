1c629f3f99b8c38cc63b82be4e9f8789a0811726
Md Hasan <212467026+hasancodecraft@users.noreply.github.com>
2026-07-03 22:43:19 +0600

    Update Module 9 folder name

--- flutter_learning_app/lib/module_09_flutter_basics/class_03_app_structure/app_structure.dart
+++ flutter_learning_app/lib/module_09_flutter_basics/class_03_app_structure/app_structure.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Class3 extends StatelessWidget{
   const Class3({super.key});
 
+@override
 Widget build(BuildContext context){
   return Scaffold(
     // Three ways to use color shades and opacity:
